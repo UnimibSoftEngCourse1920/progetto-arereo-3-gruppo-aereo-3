d736f3431c2185a72427e6e9c5c6441ccd5f60b5
Alessio Cottarelli <a.cottarelli1@campus.unimib.it>
2020-02-19 13:35:03 +0100

    Prova GUI

---
+++ Aereo3/src/gui/Home.java
@@ -0,0 +1,68 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import java.awt.Choice;
+import javax.swing.JTextField;
+import com.toedter.calendar.*;
+
+public class Home extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Home frame = new Home();
+					frame.setVisible(true);
+					frame.setResizable(false);
+					frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Home() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 596, 548);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "name_348925426532900");
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.NORTH);
+		panel_1.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+		
+		JButton btnLogIn = new JButton("LOG IN");
+		btnLogIn.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_1.add(btnLogIn);
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+	}
+
+}
