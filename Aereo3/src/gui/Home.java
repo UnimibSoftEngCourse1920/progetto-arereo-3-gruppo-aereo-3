9886e1a857eafcca1cf5db32a61af524d5bb3cdc
Alessio Cottarelli <a.cottarelli1@campus.unimib.it>
2020-02-20 17:12:13 +0100

    Provare a Fixare il controllo degli aeroporti!

--- Aereo3/src/gui/Home.java
+++ Aereo3/src/gui/Home.java
@@ -28,10 +28,38 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.Toolkit;
+import javax.swing.JRadioButton;
+import java.awt.Color;
+import javax.swing.JSeparator;
+import javax.swing.JScrollBar;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Home extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JPanel logInPanel;
+	private JPanel homePanel;
+	private JPanel panel_1;
+	private JPanel panel_3;
+	private JLabel lblAirPj;
+	private JPanel panel_4;
+	private JButton btnLogIn;
+	private JPanel panel_2;
+	private JRadioButton rdbtnSoloAndata;
+	private JRadioButton rdbtnAndataERitorno;
+	private JLabel lblAeroportoDiPartenza;
+	private JComboBox comboBox;
+	private JLabel lblAeroportoDiArrivo;
+	private JComboBox comboBox_1;
+	private JLabel lblDataDiPartenza;
+	private Component verticalStrut_6;
+	private Component verticalStrut_7;
+	private Component verticalStrut_8;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -41,7 +69,7 @@ public class Home extends JFrame {
 			public void run() {
 				try {
 					Home frame = new Home();
-					frame.setResizable(true);
+					frame.setResizable(false);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -55,68 +83,318 @@ public class Home extends JFrame {
 	 */
 	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 718, 595);
+		setBounds(100, 100, 894, 717);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLUE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new CardLayout(0, 0));
 		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, "name_358108283923800");
-		panel.setLayout(new BorderLayout(0, 0));
+		homePanel = new JPanel();
+		contentPane.add(homePanel, "name_358108283923800");
+		homePanel.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1, BorderLayout.NORTH);
-		panel_1.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+		panel_1 = new JPanel();
+		panel_1.setBackground(Color.BLUE);
+		homePanel.add(panel_1, BorderLayout.NORTH);
+		panel_1.setLayout(new BorderLayout(0, 0));
 		
-		JButton btnLogIn = new JButton("LOG IN");
-		btnLogIn.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		panel_1.add(btnLogIn);
+		panel_3 = new JPanel();
+		panel_3.setBackground(Color.BLUE);
+		panel_1.add(panel_3, BorderLayout.WEST);
 		
-		JPanel panel_2 = new JPanel();
-		panel.add(panel_2, BorderLayout.CENTER);
-		panel_2.setLayout(new MigLayout("", "[1px][305px][][27px,grow][1px]", "[][37px][][grow][grow][][][][grow][][]"));
+		lblAirPj = new JLabel("AIR PJ 3");
+		lblAirPj.setBackground(Color.BLUE);
+		lblAirPj.setForeground(Color.WHITE);
+		lblAirPj.setFont(new Font("Tw Cen MT", Font.PLAIN, 50));
+		panel_3.add(lblAirPj);
 		
-		Component verticalGlue_1 = Box.createVerticalGlue();
-		panel_2.add(verticalGlue_1, "cell 3 0");
+		panel_4 = new JPanel();
+		panel_4.setBackground(Color.BLUE);
+		panel_1.add(panel_4, BorderLayout.EAST);
 		
-		JLabel lblAeroportoDiPartenza = new JLabel("Aeroporto di Partenza:");
+		btnLogIn = new JButton("LOG IN");
+		btnLogIn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				contentPane.removeAll();
+				contentPane.add(logInPanel);
+				contentPane.repaint();
+				contentPane.revalidate();
+			}
+		});
+		btnLogIn.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		panel_4.add(btnLogIn);
+		
+		panel_2 = new JPanel();
+		panel_2.setBackground(Color.BLUE);
+		homePanel.add(panel_2, BorderLayout.CENTER);
+		GridBagLayout gbl_panel_2 = new GridBagLayout();
+		gbl_panel_2.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_2.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_2.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_2.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_2.setLayout(gbl_panel_2);
+		
+		Component verticalStrut = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut = new GridBagConstraints();
+		gbc_verticalStrut.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut.gridx = 1;
+		gbc_verticalStrut.gridy = 0;
+		panel_2.add(verticalStrut, gbc_verticalStrut);
+		
+		Component verticalStrut_1 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_1 = new GridBagConstraints();
+		gbc_verticalStrut_1.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_1.gridx = 3;
+		gbc_verticalStrut_1.gridy = 0;
+		panel_2.add(verticalStrut_1, gbc_verticalStrut_1);
+		
+		rdbtnSoloAndata = new JRadioButton("Solo Andata");
+		rdbtnSoloAndata.setForeground(Color.WHITE);
+		rdbtnSoloAndata.setBackground(Color.BLUE);
+		rdbtnSoloAndata.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		GridBagConstraints gbc_rdbtnSoloAndata = new GridBagConstraints();
+		gbc_rdbtnSoloAndata.anchor = GridBagConstraints.WEST;
+		gbc_rdbtnSoloAndata.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnSoloAndata.gridx = 1;
+		gbc_rdbtnSoloAndata.gridy = 1;
+		panel_2.add(rdbtnSoloAndata, gbc_rdbtnSoloAndata);
+		
+		rdbtnAndataERitorno = new JRadioButton("Andata e Ritorno");
+		rdbtnAndataERitorno.setForeground(Color.WHITE);
+		rdbtnAndataERitorno.setBackground(Color.BLUE);
+		rdbtnAndataERitorno.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		GridBagConstraints gbc_rdbtnAndataERitorno = new GridBagConstraints();
+		gbc_rdbtnAndataERitorno.anchor = GridBagConstraints.NORTHWEST;
+		gbc_rdbtnAndataERitorno.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnAndataERitorno.gridx = 3;
+		gbc_rdbtnAndataERitorno.gridy = 1;
+		panel_2.add(rdbtnAndataERitorno, gbc_rdbtnAndataERitorno);
+		
+		Component verticalStrut_2 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_2 = new GridBagConstraints();
+		gbc_verticalStrut_2.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_2.gridx = 1;
+		gbc_verticalStrut_2.gridy = 2;
+		panel_2.add(verticalStrut_2, gbc_verticalStrut_2);
+		
+		Component verticalStrut_4 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_4 = new GridBagConstraints();
+		gbc_verticalStrut_4.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_4.gridx = 3;
+		gbc_verticalStrut_4.gridy = 2;
+		panel_2.add(verticalStrut_4, gbc_verticalStrut_4);
+		
+		lblAeroportoDiPartenza = new JLabel("Aeroporto di Partenza:");
+		lblAeroportoDiPartenza.setForeground(Color.WHITE);
+		lblAeroportoDiPartenza.setBackground(Color.WHITE);
 		lblAeroportoDiPartenza.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		panel_2.add(lblAeroportoDiPartenza, "cell 1 1,growx,aligny top");
+		GridBagConstraints gbc_lblAeroportoDiPartenza = new GridBagConstraints();
+		gbc_lblAeroportoDiPartenza.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblAeroportoDiPartenza.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAeroportoDiPartenza.gridx = 1;
+		gbc_lblAeroportoDiPartenza.gridy = 5;
+		panel_2.add(lblAeroportoDiPartenza, gbc_lblAeroportoDiPartenza);
 		
-		JComboBox comboBox = new JComboBox();
-		panel_2.add(comboBox, "cell 3 1,growx");
+		comboBox = new JComboBox();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.gridx = 3;
+		gbc_comboBox.gridy = 5;
+		comboBox.addItem("Milano");
+		comboBox.addItem("Napoli");
+		panel_2.add(comboBox, gbc_comboBox);
 		
-		JLabel lblAeroportoDiArrivo = new JLabel("Aeroporto di Arrivo:    ");
+		Component verticalStrut_3 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_3 = new GridBagConstraints();
+		gbc_verticalStrut_3.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_3.gridx = 1;
+		gbc_verticalStrut_3.gridy = 6;
+		panel_2.add(verticalStrut_3, gbc_verticalStrut_3);
+		
+		Component verticalStrut_5 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_5 = new GridBagConstraints();
+		gbc_verticalStrut_5.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_5.gridx = 3;
+		gbc_verticalStrut_5.gridy = 6;
+		panel_2.add(verticalStrut_5, gbc_verticalStrut_5);
+		
+		lblAeroportoDiArrivo = new JLabel("Aeroporto di Arrivo:");
+		lblAeroportoDiArrivo.setForeground(Color.WHITE);
 		lblAeroportoDiArrivo.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		panel_2.add(lblAeroportoDiArrivo, "cell 1 2,growx,aligny top");
+		GridBagConstraints gbc_lblAeroportoDiArrivo = new GridBagConstraints();
+		gbc_lblAeroportoDiArrivo.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblAeroportoDiArrivo.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAeroportoDiArrivo.gridx = 1;
+		gbc_lblAeroportoDiArrivo.gridy = 8;
+		panel_2.add(lblAeroportoDiArrivo, gbc_lblAeroportoDiArrivo);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		panel_2.add(comboBox_1, "cell 3 2,growx");
+		comboBox_1 = new JComboBox();
+		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
+		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox_1.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox_1.gridx = 3;
+		gbc_comboBox_1.gridy = 8;
+		comboBox_1.addItem("Milano");
+		panel_2.add(comboBox_1, gbc_comboBox_1);
 		
-		JLabel lblDataDiPartenza = new JLabel("Data di Partenza:        ");
+		lblDataDiPartenza = new JLabel("Data di Partenza:");
+		lblDataDiPartenza.setForeground(Color.WHITE);
 		lblDataDiPartenza.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		panel_2.add(lblDataDiPartenza, "cell 1 3,alignx trailing,aligny top");
+		GridBagConstraints gbc_lblDataDiPartenza = new GridBagConstraints();
+		gbc_lblDataDiPartenza.anchor = GridBagConstraints.WEST;
+		gbc_lblDataDiPartenza.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDataDiPartenza.gridx = 1;
+		gbc_lblDataDiPartenza.gridy = 10;
+		panel_2.add(lblDataDiPartenza, gbc_lblDataDiPartenza);
 		
-		JCalendar calendar = new JCalendar();
-		panel_2.add(calendar, "cell 3 3,grow");
+		JDateChooser dateChooser = new JDateChooser();
+		GridBagConstraints gbc_dateChooser = new GridBagConstraints();
+		gbc_dateChooser.insets = new Insets(0, 0, 5, 5);
+		gbc_dateChooser.fill = GridBagConstraints.HORIZONTAL;
+		gbc_dateChooser.gridx = 3;
+		gbc_dateChooser.gridy = 10;
+		panel_2.add(dateChooser, gbc_dateChooser);
 		
-		JLabel lblDataDiArrivo = new JLabel("Data di Ritorno:");
-		lblDataDiArrivo.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		panel_2.add(lblDataDiArrivo, "cell 1 4,growx,aligny top");
+		JLabel lblDataDiRitorno = new JLabel("Data di Ritorno:");
+		lblDataDiRitorno.setForeground(Color.WHITE);
+		lblDataDiRitorno.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		GridBagConstraints gbc_lblDataDiRitorno = new GridBagConstraints();
+		gbc_lblDataDiRitorno.anchor = GridBagConstraints.WEST;
+		gbc_lblDataDiRitorno.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDataDiRitorno.gridx = 1;
+		gbc_lblDataDiRitorno.gridy = 11;
+		panel_2.add(lblDataDiRitorno, gbc_lblDataDiRitorno);
 		
-		JCalendar calendar_1 = new JCalendar();
-		panel_2.add(calendar_1, "cell 3 4,grow");
+		JDateChooser dateChooser_1 = new JDateChooser();
+		GridBagConstraints gbc_dateChooser_1 = new GridBagConstraints();
+		gbc_dateChooser_1.insets = new Insets(0, 0, 5, 5);
+		gbc_dateChooser_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_dateChooser_1.gridx = 3;
+		gbc_dateChooser_1.gridy = 11;
+		panel_2.add(dateChooser_1, gbc_dateChooser_1);
 		
-		Component verticalGlue = Box.createVerticalGlue();
-		panel_2.add(verticalGlue, "cell 3 5");
+		JButton btnCercaIlTuo_1 = new JButton("Cerca il tuo volo !");
+		btnCercaIlTuo_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(comboBox.getSelectedItem().equals(comboBox_1.getSelectedItem())) {
+					JLabel lblNewLabel_1 = new JLabel("Attenzione, gli aeroporti coincidono !");
+					lblNewLabel_1.setForeground(Color.RED);
+					lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+					GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+					gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+					gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+					gbc_lblNewLabel_1.gridx = 3;
+					gbc_lblNewLabel_1.gridy = 12;
+					panel_2.add(lblNewLabel_1, gbc_lblNewLabel_1);
+				}
+			}
+		});
+		btnCercaIlTuo_1.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		GridBagConstraints gbc_btnCercaIlTuo_1 = new GridBagConstraints();
+		gbc_btnCercaIlTuo_1.anchor = GridBagConstraints.WEST;
+		gbc_btnCercaIlTuo_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnCercaIlTuo_1.gridx = 1;
+		gbc_btnCercaIlTuo_1.gridy = 12;
+		panel_2.add(btnCercaIlTuo_1, gbc_btnCercaIlTuo_1);
 		
-		JPanel panel_3 = new JPanel();
-		panel.add(panel_3, BorderLayout.SOUTH);
+		logInPanel = new JPanel();
+		logInPanel.setBackground(Color.BLUE);
+		contentPane.add(logInPanel, "name_540380700630600");
+		GridBagLayout gbl_logInPanel = new GridBagLayout();
+		gbl_logInPanel.columnWidths = new int[]{0, 0, 0};
+		gbl_logInPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_logInPanel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_logInPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		logInPanel.setLayout(gbl_logInPanel);
 		
-		JButton btnCercaIlTuo = new JButton("Cerca il tuo Volo!");
-		btnCercaIlTuo.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		panel_3.add(btnCercaIlTuo);
+		JButton btnBack = new JButton("BACK");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				contentPane.removeAll();
+				contentPane.add(homePanel);
+				contentPane.repaint();
+				contentPane.revalidate();
+			}
+		});
+		btnBack.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		GridBagConstraints gbc_btnBack = new GridBagConstraints();
+		gbc_btnBack.anchor = GridBagConstraints.WEST;
+		gbc_btnBack.insets = new Insets(0, 0, 5, 0);
+		gbc_btnBack.gridx = 1;
+		gbc_btnBack.gridy = 0;
+		logInPanel.add(btnBack, gbc_btnBack);
+		
+		verticalStrut_6 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_6 = new GridBagConstraints();
+		gbc_verticalStrut_6.insets = new Insets(0, 0, 5, 0);
+		gbc_verticalStrut_6.gridx = 1;
+		gbc_verticalStrut_6.gridy = 1;
+		logInPanel.add(verticalStrut_6, gbc_verticalStrut_6);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setForeground(Color.WHITE);
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
+		gbc_lblEmail.insets = new Insets(0, 0, 5, 0);
+		gbc_lblEmail.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblEmail.gridx = 1;
+		gbc_lblEmail.gridy = 5;
+		logInPanel.add(lblEmail, gbc_lblEmail);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.anchor = GridBagConstraints.NORTH;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 6;
+		logInPanel.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		verticalStrut_7 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_7 = new GridBagConstraints();
+		gbc_verticalStrut_7.insets = new Insets(0, 0, 5, 0);
+		gbc_verticalStrut_7.gridx = 1;
+		gbc_verticalStrut_7.gridy = 7;
+		logInPanel.add(verticalStrut_7, gbc_verticalStrut_7);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setForeground(Color.WHITE);
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+		gbc_lblPassword.insets = new Insets(0, 0, 5, 0);
+		gbc_lblPassword.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblPassword.gridx = 1;
+		gbc_lblPassword.gridy = 8;
+		logInPanel.add(lblPassword, gbc_lblPassword);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.anchor = GridBagConstraints.NORTH;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 10;
+		logInPanel.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		verticalStrut_8 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_8 = new GridBagConstraints();
+		gbc_verticalStrut_8.insets = new Insets(0, 0, 5, 0);
+		gbc_verticalStrut_8.gridx = 1;
+		gbc_verticalStrut_8.gridy = 11;
+		logInPanel.add(verticalStrut_8, gbc_verticalStrut_8);
+		
+		JButton btnLogIn_1 = new JButton("LOG IN");
+		btnLogIn_1.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		GridBagConstraints gbc_btnLogIn_1 = new GridBagConstraints();
+		gbc_btnLogIn_1.insets = new Insets(0, 0, 5, 0);
+		gbc_btnLogIn_1.anchor = GridBagConstraints.WEST;
+		gbc_btnLogIn_1.gridx = 1;
+		gbc_btnLogIn_1.gridy = 13;
+		logInPanel.add(btnLogIn_1, gbc_btnLogIn_1);
 		
 	}
 
