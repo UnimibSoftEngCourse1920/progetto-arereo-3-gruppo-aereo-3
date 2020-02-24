48c19e2ec4810bd79a1664cfc03d0283e1499d2c
Clark <clarke2897@gmail.com>
2020-02-24 21:35:33 +0100

    inizializzazione in home

--- Aereo3/src/controller/controller.java
+++ Aereo3/src/controller/controller.java
@@ -1,11 +1,13 @@
 package controller;
+import gui.Home;
 
 public class controller {
 
 	public static void main (String... args) {
-		
-		
+
 		System.out.println("funziona!");
+		Home.main(args);
+		
 		
 	}
 	
