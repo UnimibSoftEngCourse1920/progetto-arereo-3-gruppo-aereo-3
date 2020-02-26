2f4c5f964921e9fcc31358eecbff53ae0e0ff635
Alessio <alessio.fiore6@gmail.com>
2020-02-26 11:21:24 +0100

    Gestione chiave primaria Cliente e ClienteFedele

--- Aereo3/src/dominio/Cliente.java
+++ Aereo3/src/dominio/Cliente.java
@@ -9,104 +9,122 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
-
-
 @Entity
-@Table(name="cliente")
+@Table(name = "cliente")
 public class Cliente {
 
-	//TODO: sistemare il database, diversificare il cliente fedele
 	@Id
-	@Column(name="cod_cliente")
+	@Column(name = "cod_cliente")
 	private String codCliente;
-	
-	@Column(name="nome")
+
+	@Column(name = "nome")
 	private String nome;
-	
-	@Column(name="cognome")
+
+	@Column(name = "cognome")
 	private String cognome;
-	
-	@Column(name="data_di_nascita")
+
+	@Column(name = "data_di_nascita")
 	private LocalDateTime dataDiNascita;
-	
-	@Column(name="email")
+
+	@Column(name = "email")
 	private String email;
-	
-	@Column(name="indirizzo")
+
+	@Column(name = "indirizzo")
 	private String indirizzo;
-	//			private CartaDiCredito cartaDiCredito;
+	
+	@Column(name="fedele")
+	private boolean fedele = false;
+
+	//data in cui il cliente è diventato infedele (dopo due anni che non acquista biglietti)
+	@Column(name = "infedele")
+	private LocalDateTime infedele;
 
 	public String getCodCliente() {
 		return codCliente;
 	}
 
+	public boolean isFedele() {
+		return fedele;
+	}
+
+	public void setFedele(boolean fedele) {
+		this.fedele = fedele;
+	}
+
+	public LocalDateTime getInfedele() {
+		return infedele;
+	}
+
+	public void setInfedele(LocalDateTime infedele) {
+		this.infedele = infedele;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
 	public String getCognome() {
 		return cognome;
 	}
+
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
 
-
 	public LocalDateTime getDataDiNascita() {
 		return dataDiNascita;
 	}
+
 	public void setDataDiNascita(LocalDateTime dataDiNascita) {
 		this.dataDiNascita = dataDiNascita;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getIndirizzo() {
 		return indirizzo;
 	}
+
 	public void setIndirizzo(String indirizzo) {
 		this.indirizzo = indirizzo;
 	}
+
 	/*****************************************/
-	public void consultaVolo (String partenza, String destinazione, LocalTime orario) {
+	public void consultaVolo(String partenza, String destinazione, LocalTime orario) {
 
 	}
 
 	/*****************************************/
 	public void prenotaVolo(Volo volo) {
-		
+
 	}
 
 	/******************************************/
-	
+
 	public void paga(Prenotazione prenotazione) {
-		
+
 	}
-	
+
 	/*****************************************/
-	public void modificaPrenotazione (Prenotazione prenotazione, LocalTime nuovoOrario) {
-		
+	public void modificaPrenotazione(Prenotazione prenotazione, LocalTime nuovoOrario) {
+
 	}
 
 	public static boolean consultaVolo(Volo voloDaControllare) {
-	
-		
+
 		return false;
 	}
 
-	
 //	/****************************************/
 //	/**
 //	 * @throws ClassNotFoundException ***************************************/
@@ -202,9 +220,5 @@ public class Cliente {
 //	
 
 	/****************************************/
-	
-	
 
-	
 }
-
